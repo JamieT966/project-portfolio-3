de26546320c1da21866fd79b8007610284879701
JamieT966 <jamietarpey1@gmail.com>
2022-03-11 19:48:28 +0000

    Added intro, movie choices and input field

--- run.py
+++ run.py
@@ -16,4 +16,18 @@ movies = SHEET.worksheet('movies')
 
 movie_list = movies.get_all_values()
 
-print(movie_list)
+def movie_choice():
+    """
+    Introduction, ticket prices and max no. of tickets per transaction.
+    Presents user with choice of 4 movies and asks user for input.
+    """
+    print('Welcome to Love Movies\n')
+    print('Please select the movie you would like to see. All tickets cost €10.')
+    print('Max number of tickets per transaction: 6.\n')
+    print('a. The Batman')
+    print('b. Star Wars: The Empire Strikes Back')
+    print('c. Lord of the Rings: The Two Towers')
+    print('d. Iron Man\n')
+    movie_choice = input('Enter Movie Choice by entering a, b, c or d.\n')
+
+movie_choice()
