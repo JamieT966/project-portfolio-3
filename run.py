edf7f6ce5385ea425bbc2bf4ae58841296e65187
JamieT966 <jamietarpey1@gmail.com>
2022-03-12 11:35:02 +0000

    Added while loop so program doesn't end if incorrect input is given

--- run.py
+++ run.py
@@ -28,22 +28,23 @@ def movie_choice():
     print('2. Star Wars: The Empire Strikes Back')
     print('3. Lord of the Rings: The Two Towers')
     print('4. Iron Man\n')
-    movie_choice = input('Enter Movie Choice by entering 1, 2, 3 or 4.\n')
+    while True:
+        movie_choice = input('Enter Movie Choice by entering 1, 2, 3 or 4.\n')
 
-    if movie_choice == '1':
-        print('Batman')
-        return 'The Batman'
-    elif movie_choice == '2':
-        print('Star Wars')
-        return 'Star Wars: The Empire Strikes Back'
-    elif movie_choice == '3':
-        print('LOTR')
-        return 'Lord of the Rings: The Two Towers'
-    elif movie_choice == '4':
-        print('Iron Man')
-        return 'Iron Man'
-    else:
-        print('Sorry, we were looking for a number between 1 and 4.')
+        if movie_choice == '1':
+            print('Batman')
+            return 'The Batman'
+        elif movie_choice == '2':
+            print('Star Wars')
+            return 'Star Wars: The Empire Strikes Back'
+        elif movie_choice == '3':
+            print('LOTR')
+            return 'Lord of the Rings: The Two Towers'
+        elif movie_choice == '4':
+            print('Iron Man')
+            return 'Iron Man'
+        else:
+            print('Sorry, we were looking for a number between 1 and 4.\n')
 
 
 def main():
