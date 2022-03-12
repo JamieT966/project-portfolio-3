66378252ad8307a82919fb63140adb78500738bc
JamieT966 <jamietarpey1@gmail.com>
2022-03-12 17:39:03 +0000

    A bit lost, will come back to this later

--- run.py
+++ run.py
@@ -36,20 +36,24 @@ def movie_choice():
         if movie_select == '1':
             print('Batman\n')
             return 'The Batman'
+            number_of_seats(0)
         elif movie_select == '2':
             print('Star Wars\n')
             return 'Star Wars: The Empire Strikes Back'
+            number_of_seats(1)
         elif movie_select == '3':
             print('LOTR\n')
             return 'Lord of the Rings: The Two Towers'
+            number_of_seats(2)
         elif movie_select == '4':
             print('Iron Man\n')
             return 'Iron Man'
+            number_of_seats(3)
         else:
             print('Sorry, we were looking for a number between 1 and 4.\n')
 
 
-def number_of_seats():
+def number_of_seats(index):
     """
 
     Asks user for input. Number of seats required up to a maximum of 6.
@@ -90,11 +94,13 @@ def check_available_seats(seat):
     print('Checking available seats...')
     movies = SHEET.worksheet('movies')
     movies.append_row(seat)
+    print('Seats selected.')
 
 
 def main():
     movie_select = movie_choice()
-    seat_choice = number_of_seats()
+    seat_choice = number_of_seats_batman()
+    #seat_choice = number_of_seats_star_wars()
     check_available_seats(seat_choice)
     print(movie_select)
     print(seat_choice)
