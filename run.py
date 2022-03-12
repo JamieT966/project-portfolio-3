37c4b2cbc1c2ce5b050c34cde8d30dd7e1a28593
JamieT966 <jamietarpey1@gmail.com>
2022-03-12 12:05:42 +0000

    Added function that allows user to select number of seats up to a max of 6

--- run.py
+++ run.py
@@ -28,26 +28,57 @@ def movie_choice():
     print('2. Star Wars: The Empire Strikes Back')
     print('3. Lord of the Rings: The Two Towers')
     print('4. Iron Man\n')
+
     while True:
         movie_choice = input('Enter Movie Choice by entering 1, 2, 3 or 4.\n')
 
         if movie_choice == '1':
-            print('Batman')
+            print('Batman\n')
             return 'The Batman'
         elif movie_choice == '2':
-            print('Star Wars')
+            print('Star Wars\n')
             return 'Star Wars: The Empire Strikes Back'
         elif movie_choice == '3':
-            print('LOTR')
+            print('LOTR\n')
             return 'Lord of the Rings: The Two Towers'
         elif movie_choice == '4':
-            print('Iron Man')
+            print('Iron Man\n')
             return 'Iron Man'
         else:
             print('Sorry, we were looking for a number between 1 and 4.\n')
 
 
+def number_of_seats():
+    print('Please Choose the number of seats you would like.')
+    print('Remember, the maximum number of seats is 6.\n')
+
+    while True:
+        seat_choice = input('Number of Seats:')
+
+        if seat_choice == '1':
+            print('1 Seat Chosen')
+            return '1'
+        elif seat_choice == '2':
+            print('2 Seats Chosen')
+            return '2'
+        elif seat_choice == '3':
+            print('3 Seats Chosen')
+            return '3'
+        elif seat_choice == '4':
+            print('4 Seats Chosen')
+            return '4'
+        elif seat_choice == '5':
+            print('5 Seats Chosen')
+            return '5'
+        elif seat_choice == '6':
+            print('6 Seats Chosen')
+            return '6'
+        else:
+            print('Sorry, we were looking for a number between 1 and 6.\n')
+
 def main():
     movie_choice()
-
+    number_of_seats()   
+    print({movie_choice})
 main()
+
